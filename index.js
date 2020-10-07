97e3138536fbee980ede4e41cb48e99bc07898b6
jhidalgo990 <josehidalgo990@gmail.com>
2020-10-06 21:00:46 -0400

    new design

--- index.js
+++ index.js
@@ -33,12 +33,12 @@ window.addEventListener("DOMContentLoaded", async (e) => {
       task.id = doc.id;
       //   console.log(task);
 
-      taskContainer.innerHTML += `<div class="form">
+      taskContainer.innerHTML += `<div class="cards">
         <h5>${task.title}</h5>
         <p>${task.description}</p>
-        <div>
-            <button class="" data-id="${task.id}">Delete</button>
-            <button class="" data-id="${task.id}">Edit</button>
+        <div class="cards-button">
+            <button class="btn-delete" data-id="${task.id}">Delete</button>
+            <button class="btn-edit" data-id="${task.id}">Edit</button>
         </div>
         </>`;
 
