ff00799e70f867cad3c73fd67b0db84d127057a8
Jose Hidalgo <josehidalgo990@gmail.com>
2020-10-09 22:20:01 -0400

    dark mode ready?

--- actions.js
+++ actions.js
@@ -1,25 +1,5 @@
-let isDark = false;
 
-function sun() {
-  document.getElementById("moon-sun").src = "./img/sun.svg";
-  document.getElementById("moon-sun").style.filter = "invert(0)";
-  isDark = true;
-}
-function moon() {
-  document.getElementById("moon-sun").src = "./img/luna.svg";
-  document.getElementById("moon-sun").style.filter = "invert(0)";
-  isDark = false;
-}
-function sun2() {
-  document.getElementById("moon-sun").src = "./img/luna.svg";
-  document.getElementById("moon-sun").style.filter = "invert(0)";
-  isDark = false;
-}
-function moon2() {
-  document.getElementById("moon-sun").src = "./img/sun.svg";
-  document.getElementById("moon-sun").style.filter = "invert(0)";
-  isDark = true;
-}
+let isDark = true;
 
 document.querySelector(".plus").addEventListener("click", () => {
   document.querySelector(".form").classList.toggle("active");
@@ -27,40 +7,14 @@ document.querySelector(".plus").addEventListener("click", () => {
 });
 
 document.querySelector(".dark-mode").addEventListener("click", () => {
-  /*
-   document.documentElement.classList.toggle("dark");
-   if (document.documentElement.classList == ""){
-     moon();
-    }else {
-      sun();
-    }
-    */
-  /*document.documentElement.classList.toggle("dark");
-  if (document.getElementById("moon-sun").src === "./img/luna.svg") {
-    moon();
-  }else {
-    sun();
-  }*/
-  if (!isDark) {
-    moon2();
-    document.documentElement.classList.remove("light");
-    document.documentElement.classList.add("dark");
-    console.log("ahora es oscuro");
-  } else {
-    sun2();
-    document.documentElement.classList.remove("dark");
+  if (isDark) {
+    //moon2();
     document.documentElement.classList.add("light");
-    console.log("ahora es luz");
+    document.getElementById("moon-sun").src = "./img/luna.svg";
+    isDark = false;
+  } else {
+    document.documentElement.classList.remove("light");
+    document.getElementById("moon-sun").src = "./img/sun.svg";
+    isDark = true;
   }
 });
-
-console.log(window.matchMedia("prefers-color-scheme: dark"));
-
-if (!window.matchMedia("prefers-color-scheme: dark")) {
-  sun();
-  console.log("dark y muestro el sol");
-} else {
-  moon();
-  console.log("light y muestro la luna");
-}
-console.log(isDark);
